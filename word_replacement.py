8c0da3c5ba166fe5b164293348b947d3d67cc3b7
Nedzma <brkicnedzma@gmail.com>
2023-07-27 20:39:43 +0200

    Added word_replacement.py

---
+++ word_replacement.py
@@ -0,0 +1,9 @@
+def replace_word():
+    
+    str = "Hi guys, i am nedzma, and hi hi hi hi"
+    print(str)
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word to replace: ")
+    print(str.replace(word_to_replace, word_replacement))
+    
+replace_word()
